f3c511f3da87481bd3b2f20c46d74a118ce6c498
Trevor Whitney <trevorjwhitney@gmail.com>
2012-02-01 15:58:06 -0700

    modified script to work backwards

--- hw1/h1.rb
+++ hw1/h1.rb
@@ -1,4 +1,4 @@
-2.times do |num|
-  system("mpiexec -np #{num + 1} --hostfile hostfile parallel_pi >> pi_#{num + 1}.txt")
-  system("mpiexec -np #{num + 1} --hostfile hostfile circuits >> circuits_#{num + 1}.txt")
+16.times do |num|
+  system("mpiexec -np #{16 - num} --hostfile hostfile parallel_pi >> pi_#{num + 1}.txt")
+  system("mpiexec -np #{16 - num} --hostfile hostfile circuits >> circuits_#{num + 1}.txt")
 end
