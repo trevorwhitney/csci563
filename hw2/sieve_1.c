24bae2af09c85fc442f9ac76959aa7f0f41529b8
Trevor Whitney <trevorjwhitney@gmail.com>
2012-02-21 18:00:34 -0700

    decomosed sieve_1.c into smaller functions

--- hw2/sieve_1.c
+++ hw2/sieve_1.c
@@ -2,6 +2,8 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+//Macros
 #define MIN(a,b)  ((a)<(b)?(a):(b))
 
 #define BLOCK_LOW(id,p,n)  (( (id)*(n)/(p) % 2 == 0)?( ((id)*(n)/(p)) ):( (id)*(n)/(p) - 1 ))
@@ -18,6 +20,14 @@
 #define GLOBAL_TO_LOCAL(value, low_value) \
         ( (ceil((float)value/2) - 2) - (ceil((float)low_value/2) - 2) )
 
+//function prototypes
+void decompose_data(int, int, int, int*, int*, int*);
+void processor_count_check(int, int, int);
+char* allocate_memory(int);
+void find_first_index(int*, int, int);
+int count_local_primes(char*, int);
+
+
 int main (int argc, char *argv[])
 {
   //define variables
@@ -28,8 +38,6 @@ int main (int argc, char *argv[])
   int low_value;
   int high_value;
   int size;
-  int array_size;
-  int proc0_size;
   char* marked;
   int index;
   int prime;
@@ -37,8 +45,6 @@ int main (int argc, char *argv[])
   int count;
   int global_count;
   int i;
-  int value;
-  int mod_prime;
 
   //Initialize MPI
   MPI_Init (&argc, &argv);
@@ -58,57 +64,20 @@ int main (int argc, char *argv[])
   //N represents the number up to which we need to calculate primes
   n = atoi(argv[1]);
 
-  //Low and high values for each processor, from 3 to n
-  low_value = 3 + BLOCK_LOW(id,p,n-1);
-  high_value = 3 + BLOCK_HIGH(id,p,n-1);
-  size = BLOCK_SIZE(id,p,n-1);
-
-  //remove even integers
-  array_size = size/2;
-  
-  //largest prime is sqrt(n), so first processor has all primes if
-  //p is less than sqrt(n). We need to check we don't have more processors
-  //than we need.
-  proc0_size = (n-1)/p;
-  if ((2 + proc0_size) < (int) sqrt((double) n)) {
-    if (!id) printf ("Too many processes\n");
-    MPI_Finalize();
-    exit (1);
-  }
-
-  //allocate memory for block, error if unable to
-  marked = (char *) malloc (array_size);
-  if (marked == NULL) {
-    printf ("Cannot allocate enough memory\n");
-    MPI_Finalize();
-    exit (1);
-  }
+  decompose_data(id, p, n, &low_value, &high_value, &size);
+  processor_count_check(p, n, id);
 
   /* Begin Sieve of Eratosthenes Algorithm */
-  //First fill marked[] with zero/false for all items in block
-  for (i = 0; i < array_size; i++) marked[i] = 0;
-  
+  marked = allocate_memory(size);
   if (!id) index = 0;
   
   //first prime is 3, since we're skipping 2
   prime = 3;
   do {
-    if (prime * prime > low_value) {
-      //If low_value is less than prime*prime, then we need to start at prime*prime
-      first = GLOBAL_TO_LOCAL(prime*prime, low_value);
-    }
-    else {
-       if (!(low_value % prime)) first = 0;
-       else {
-         mod_prime = low_value % prime;
-         if (mod_prime % 2 == 0) value = low_value - mod_prime + 2*prime;
-         else value = low_value - mod_prime + prime;
-         first = GLOBAL_TO_LOCAL(value, low_value);
-       }
-    }
+    find_first_index(&first, prime, low_value);
 
     //increment by prime, marking the non-primes with 1, or 'marked'
-    for (i = first; i < array_size; i += prime) {
+    for (i = first; i < size; i += prime) {
       marked[i] = 1;
     }
 
@@ -117,19 +86,13 @@ int main (int argc, char *argv[])
        while (marked[++index]);
        prime = index*2 + 3;
     }
-
     //broadcast new prime to other processors
     MPI_Bcast (&prime,  1, MPI_INT, 0, MPI_COMM_WORLD);
   } while (prime * prime <= n);
   /* End Sieve of Eratosthenes Algorithm */
 
-  /*Begin count of primes */
-  count = 0;
-
-  //for all elements in block, if prime is 1/true, increment count
-  for (i = 0; i < array_size; i++) {
-    if (!marked[i]) count++;
-  }
+  
+  count = count_local_primes(marked, size);
   
   //Sum count of primes from each process
   MPI_Reduce (&count, &global_count, 1, MPI_INT, MPI_SUM,
@@ -151,3 +114,78 @@ int main (int argc, char *argv[])
   MPI_Finalize();
   return 0;
 }
+
+void decompose_data(int id, int p, int n, int *low_value, int *high_value, int *size) {
+  //Low and high values for each processor, from 3 to n
+  int true_size;
+
+  *low_value = 3 + BLOCK_LOW(id,p,n-1);
+  *high_value = 3 + BLOCK_HIGH(id,p,n-1);
+  true_size = BLOCK_SIZE(id,p,n-1);
+
+  //remove even integers
+  *size = true_size/2;
+}
+
+void processor_count_check(int p, int n, int id) {
+  //largest prime is sqrt(n), so first processor has all primes if
+  //p is less than sqrt(n). We need to check we don't have more processors
+  //than we need.
+  int proc0_size;
+
+  proc0_size = (n-1)/p;
+  if ((2 + proc0_size) < (int) sqrt((double) n)) {
+    if (!id) printf ("Too many processes\n");
+    MPI_Finalize();
+    exit (1);
+  }
+}
+
+char* allocate_memory(int size) {
+  //allocate memory for block, error if unable to
+  char* marked;
+  int i;
+  
+  marked = (char *) malloc (size);
+  if (marked == NULL) {
+    printf ("Cannot allocate enough memory\n");
+    MPI_Finalize();
+    exit (1);
+  }
+
+  //Fill marked[] with zero/false for all items in block
+  for (i = 0; i < size; i++) marked[i] = 0;
+  return marked;
+}
+
+void find_first_index(int *first, int prime, int low_value) {
+  int mod_prime;
+  int value;
+
+  if (prime * prime > low_value) {
+      //If low_value is less than prime*prime, then we need to start at prime*prime
+      *first = GLOBAL_TO_LOCAL(prime*prime, low_value);
+  }
+  else {
+    if (!(low_value % prime)) *first = 0;
+    else {
+      mod_prime = low_value % prime;
+      if (mod_prime % 2 == 0) value = low_value - mod_prime + 2*prime;
+      else value = low_value - mod_prime + prime;
+      *first = GLOBAL_TO_LOCAL(value, low_value);
+    }
+  }
+}
+
+int count_local_primes(char* marked, int size) {
+  int count = 0;
+  int i;
+
+  //for all elements in block, if prime is 1/true, increment count
+  for (i = 0; i < size; i++) {
+    if (!marked[i]) count++;
+  }
+
+  return count;
+}
+  
