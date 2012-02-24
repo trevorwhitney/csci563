38de1ba71a5494658ac54b7e177086d46732382f
Trevor Whitney <trevorjwhitney@gmail.com>
2012-02-24 10:19:54 -0700

    updated script for hw2 to run code

--- hw2/h2.rb
+++ hw2/h2.rb
@@ -1,4 +1,5 @@
-16.times do |num|
-  system("mpiexec -np #{16 - num} --hostfile hostfile ./parallel_pi >> results/pi_#{16 - num}.txt")
-  system("mpiexec -np #{16 - num} --hostfile hostfile ./circuits >> results/circuits_#{16 -num}.txt")
+8.times do |num|
+  system("mpiexec -np #{8 - num} --hostfile hostfile sieve_0 1000 >> results/0_#{8 - num}.txt")
+  system("mpiexec -np #{8 - num} --hostfile hostfile sieve_1 1000 >> results/1_#{8 -num}.txt")
+  system("mpiexec -np #{8 - num} --hostfile hostfile sieve_2 1000 >> results/2_#{8 -num}.txt")
 end
